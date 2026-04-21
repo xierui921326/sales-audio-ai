ffc22c41c68db38b00a6c9848789418ec0d58c40
谢瑞 <1115775006@qq.com>
2026-04-21 16:29:28 +0800

    fix: 修复bug

--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? './' : '/',
   plugins: [react()],
   clearScreen: false,
   server: {
     strictPort: true,
-    port: 5173,
+    host: '127.0.0.1',
+    port: 4173,
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
   envPrefix: ['VITE_', 'TAURI_'],
-})
+}))
